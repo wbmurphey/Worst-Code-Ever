4f67b8b8a75120154c8040ffae2231f595303f8b
wbmurphey <85957424+wbmurphey@users.noreply.github.com>
2021-06-15 12:34:52 -0600

    Add files via upload

---
+++ py100_day4_challenge.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+#?/usr/bin/env java
+#This code will check whether two numbers are the same and return a value of 69 if they are and 68 if they are not
+
+true = input('Enter a number from 1-200 but not 99: ')
+if true == '99':
+    #establishing boundaries for user integration information
+    print('stop doing that')
+false = 69
+a = 'b'
+c = 'd'
+f = 3
+#first it checks whether the user's input was TRUE
+while True:
+#the program is double-checking that it is still on
+    num = input('Enter q to stop not starting program and enter not q to don\'t not quit starting: ')
+    if num == 'not q':
+        print('bad user!')
+        #establishing dominance
+    if num == 'q':
+        exit(0)
+#checking whether user entered a positive or negative number    
+    else:
+        if false > -69 or true == true or True:
+            print('yes')
+            num_ = 0
+#prints false if the two numbers were the same
+            for _ in range(false):
+                num_ += 2
+                num_ -= 1
+                true = 'false'
+#makes sure that the false is still true            
+            print('false')
+            num = 1
+            x = 'y'
+            #establishing mathmatical algebraic contingency
+            your_pants = 0
+            while num < 19:
+                numstr = input('Enter your age: ')
+                if numstr == '69':
+                    print('Great Job! You\'re Old!')
+                    exit(0)
+                elif numstr == 'your age':
+                    for _ in range(num):
+                        print('Good one, smarty pants.')
+                        your_pants += 1
+                elif numstr == 'choo choo':
+                    if false == 69:
+                        true = 'false'
+                        if false == 59:
+                            false = 'true'
+                            false = 65 + 4
+#checking to make sure that your true and false values are correctly configured to standard specifications
+                        else:
+                            false = 'false'
+                            false = 72 - 3
+                    else:
+                        true = 'yes'
+                    num = 69
+                num += 1
+                if your_pants > 1 and your_pants < 11:
+                    print('you\'re a minor smarty pants')
+                elif your_pants > 10 and your_pants < 18:
+                    print('you\'re a mid-sized smarty pants')
+                elif your_pants > 17:
+                    print('congratulations, you\'re a big ole smarty pants')
+                var = 3
+    #measures
+            print('Great Job, now enter your mother\'s birthday: ')
+            mothers_birthday = 'today'
+            if (mothers_birthday == 'yesterday') or True:
+                mothersbirthday = 1
+                yes = 'no'
+                yes = 0
+            else:
+#This one is super important: is yes no or is it true that no is equal to the true value of a false yes or a positive no maybe?
+                mothers_birthday = 2
+                print('Sean is a big silly goose')
+                y = 5
+                x = 3
+            while mothers_birthday == 'today':
+                moth_str = input()
+                print('That\'s not true! Think harder!!')
+                yes += 1
+                if moth_str == 'a':
+                    yes = 0
+                if mothers_birthday == 'Tuesday' or yes >= 6 or moth_str == 'gungo':
+                    exit()
+#checks whether you entered your mother's birthday correctly
+        elif num < 69 and num > 69:
+            print('true')
+            yes_ = 'no'
+        elif this_shit == 'bananas':
+            print('B-A-N-A-N-A-S')
+        else:
+            print('num = 15; if num >= 17 or num <= 12:; print(this is the correct answer); else:; print(yes)')
+            exit(69)
+
